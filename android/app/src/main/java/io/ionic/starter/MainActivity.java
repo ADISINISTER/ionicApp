3b70fd4be4c2eec7aba7deff345e8a2f50cc63e8
ADISINISTER <appy3503@gmail.com>
2025-03-19 18:15:27 +0530

    first commit

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,10 @@
+package io.ionic.starter;
+
+import com.clevertap.android.sdk.CleverTapAPI;
+import com.getcapacitor.BridgeActivity;
+
+
+public class MainActivity extends BridgeActivity {
+//  CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.DEBUG);
+//  CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.VERBOSE);
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CleverTap} from '@awesome-cordova-plugins/clevertap'
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,45 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  message = '';
 
-  constructor() {}
+  constructor() {
+    CleverTap.enableDeviceNetworkInfoReporting(true);
+    // ✅ Request permission using Promises (No async/await)
+    Geolocation.requestPermissions()
+      .then((permStatus) => {
+        if (permStatus.location === 'granted') {
+          return Geolocation.getCurrentPosition();
+        } else {
+          console.log("Location permission denied");
+          return null;
+        }
+      })
+      .then((position) => {
+        if (position) {
+          console.log("Device Location:", position.coords.latitude, position.coords.longitude);
+          
+          CleverTap.getLocation();
+          // ✅ Set location in CleverTap
+          CleverTap.setLocation(position.coords.latitude, position.coords.longitude);
+        }
+      })
+      .catch((error) => {
+        console.error("Error getting location:", error);
+      });
 
+    // CleverTap.getLocation();
+    // CleverTap.setLocation(location.lat, location.lon);
+    // console.log("Location: ", location);
+    // if (location) {
+    //   console.log("Location: ", location.lat, location.lon);
+    // }
+    CleverTap.setDebugLevel(3);
+  }
+
+  onClick() {
+    this.message = 'Button clicked!';
+    CleverTap.recordEventWithName("foo");
+    CleverTap.registerPush();
+}
 }
